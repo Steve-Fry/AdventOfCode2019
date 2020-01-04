215a9d12337202b2655f5612833ada785eaa7bf1
Steve <sfry89@gmail.com>
2020-01-04 00:14:50 +0000

    Add test case for day 5 part 2 solution.

---
+++ Advent of Code/SharedLibrary/IntcodeVirtualMachine/Tests/Test_IntCodeMachine_Day5_Part2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Advent_of_Code.SharedLibrary.IntcodeVirtualMachine.Input_OutputProviders;
+
+namespace Advent_of_Code.SharedLibrary.IntcodeVM.Tests
+{
+    public class Test_IntCodeMachine_Day5_Part2
+    {
+
+        [Fact]
+        public void ShouldCorrectlyCalculatePart2()
+        {
+            string filename = @"Day5Part1.txt";
+            List<int> program = SharedLibrary.FileParser.GetIntCodeFromFile(@"..\..\..\Day 05\input.txt");
+            int expected = 3419022;
+
+            IntcodeVirtualMachine intcodeVirtualMachine = new IntcodeVirtualMachine(program, new StaticInputProvider(new List<int>() { 5 }), new FileOutputProvider(filename));
+            intcodeVirtualMachine.Run();
+
+            string actual = System.IO.File.ReadAllText(filename);
+            Assert.Equal(expected.ToString(), actual);
+        }
+    }
+}
+
+
