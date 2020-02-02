8a5390e6decbee6dd423fe6dc498c67b129afb69
Steve <sfry89@gmail.com>
2020-02-02 20:48:54 +0000

    Add Tests and working solution for Day9 Part1 and Part2

--- Advent of Code/Day 09/Day 09.md
+++ Advent of Code/Day 09/Day 09.md
@@ -1,8 +1,17 @@
 ﻿# Day 09
 
 ## Part 1
+I started by refactoring the intcodeVM, which was looking quite overly complex by this point. 
+There is clearly still work to do, but I'm happier with the overall model model now. 
 
+Implementing the large int was an exercise of repetitive copy-paste, I could have been kinder with 
+respecting the API provided to the previous solutions, but just went for the (mostly) brute force approach. 
 
+Once refactored, the implemention went smoothly with only a bit of debugging caused by misreading the instruction 
+for how to modify the relative base. You should add the value of the first parameter, not replace the relative base 
+with that value. 
 
+Once the tests were passing part 1 didn't cause any problems. 
 
 ## Part 2
+I was surprised by this one, it seemed suspiciously easy. 
--- Advent of Code/Day 09/Tests/Test_Day9_Solutions.cs
+++ Advent of Code/Day 09/Tests/Test_Day9_Solutions.cs
@@ -1,52 +1,38 @@
 ﻿using Advent_of_Code.SharedLibrary.IntcodeVirtualMachine;
 using Advent_of_Code.SharedLibrary.IntcodeVirtualMachine.Input_OutputProviders;
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
+using Xunit;
 
-namespace Advent_of_Code.Day_09
+namespace Advent_of_Code.Day_09.Tests
 {
-    class Day_09
+    public class Test_Day9_Solutions
     {
-
-        public void Run()
+        [Fact]
+        public void ShouldCorrectlyCalculatePart1Solution()
         {
-            Stopwatch stopWatch = Stopwatch.StartNew();
+            long expected = 3533056970;
 
-            Console.WriteLine($"=====Day 9, part 1=====");
-            Console.WriteLine($"Solution: {GetPart1Solution()}");
-
-            Console.WriteLine($"=====Day 9, part 2=====");
-            Console.WriteLine($"Solution: {GetPart2Solution()}");
-
-            stopWatch.Stop();
-            Console.WriteLine();
-            Console.WriteLine($"Day 9 completed in {stopWatch.ElapsedMilliseconds} ms");
-            Console.WriteLine();
-            Console.WriteLine();
-
-        }
-
-        private long GetPart1Solution()
-        {
             List<int> program = SharedLibrary.FileParser.GetIntCodeFromFile(@"..\..\..\Day 09\Day9Input.txt");
             Queue<long> outputQueue = new Queue<long>();
-            
+
             IntcodeVirtualMachine intcodeVirtualMachine = new IntcodeVirtualMachine(program, new StaticInputProvider(new List<long> { 1 }), new QueueOutputProvider(outputQueue));
             intcodeVirtualMachine.Run();
-            return outputQueue.Dequeue();
+
+            Assert.Equal(expected, outputQueue.Dequeue());
         }
 
-        private long GetPart2Solution()
+        [Fact]
+        public void ShouldCorrectlyCalculatePart2Solution()
         {
+            long expected = 72852;
+
             List<int> program = SharedLibrary.FileParser.GetIntCodeFromFile(@"..\..\..\Day 09\Day9Input.txt");
             Queue<long> outputQueue = new Queue<long>();
 
             IntcodeVirtualMachine intcodeVirtualMachine = new IntcodeVirtualMachine(program, new StaticInputProvider(new List<long> { 2 }), new QueueOutputProvider(outputQueue));
             intcodeVirtualMachine.Run();
-            return outputQueue.Dequeue();
+
+            Assert.Equal(expected, outputQueue.Dequeue());
         }
     }
 }
