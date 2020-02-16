7268509304ea7f19811430938c963467f0e6ce20
Steve <sfry89@gmail.com>
2020-02-16 00:00:39 +0000

    Add Method doc strings

--- Advent of Code/Day 10/Asteroid.cs
+++ Advent of Code/Day 10/Asteroid.cs
@@ -14,7 +14,12 @@ namespace Advent_of_Code.Day_10
             Y = y;
         }
 
-        public double GetSlope(Asteroid asteroid)
+        /// <summary>
+        /// Get the angle made between the source and this object with respects to the X+ axis.
+        /// return this angle in radians. 
+        /// </summary>
+        /// <param name="asteroid"></param>
+        public double GetAngle(Asteroid asteroid)
         {
             double deltaY = asteroid.Y - this.Y;
             double deltaX = asteroid.X - this.X;
--- Advent of Code/Day 10/AsteroidMap.cs
+++ Advent of Code/Day 10/AsteroidMap.cs
@@ -16,6 +16,9 @@ namespace Advent_of_Code.Day_10
             ParseMapFile(filename);
         }
 
+        /// <summary>
+        /// Parses the mapfile into a list of Asteroid objects. 
+        /// </summary>
         internal void ParseMapFile(string filename)
         {
             int i = 0;
@@ -34,6 +37,17 @@ namespace Advent_of_Code.Day_10
             if (Asteroids.Count == 0) { throw new FileLoadException("No asteroids loaded from file."); }
         }
 
+        /// <summary>
+        /// When passed an asteroid object, the method simulates a laser rotating clockwise from 12 oclock on this object. 
+        /// each asteroid the beam falls upon is destroyed, but asteroids behind the first Asteroid are untouched 
+        /// until the next rotation. The method returns when all other asteroids have been destroyed.
+        /// Note that in the dictionary 
+        ///     9oclock = -PI
+        ///     12oclock = -1/2PI
+        ///     3oclock = 0
+        /// Therefore, we must skip the first 1/2 PI and start at 12oclock as per the AOC spec. 
+        /// </summary>
+        /// <param name="sourceAsteroid"></param>
         internal IEnumerable<Asteroid> GetOrderOfDestruction(Asteroid sourceAsteroid)
         {
             SortedDictionary<double, SortedList<double, Asteroid>> anglesWithNeighbours = GetDictionaryOfNeighbours(sourceAsteroid);
@@ -48,26 +62,33 @@ namespace Advent_of_Code.Day_10
                 {
                     // Angles will be in the range +/- PI
                     // If we started from -PI we would be 9 o'clock, we actually want to start at 12 o'clock
-                    if (firstRun && angle < (Math.PI * -0.5)) { continue; } 
-                    else if (firstRun && angle > Math.PI * - 0.5) { firstRun = false; } // Once we pass -1/2 Pi for the first time - its fair game. 
+                    if (firstRun && angle < (Math.PI * -0.5)) { continue; }
+                    else if (firstRun && angle > Math.PI * -0.5) { firstRun = false; } // Once we pass -1/2 Pi for the first time - its fair game. 
 
                     if (anglesWithNeighbours[angle].Count > 0)
                     {
                         yield return anglesWithNeighbours[angle].Values[0];
                         anglesWithNeighbours[angle].RemoveAt(0);
-                        somethingNukedThisRound = true; 
+                        somethingNukedThisRound = true;
                     }
                 }
             }
         }
 
+        /// <summary>
+        /// A helper function that generates a SortedDictionary containing all valid angles between the source object
+        /// and any neighbour Asteroids. Each angle in the SortedDictionary contains a SortedList sorted on the distance 
+        /// between the source and neighrbour object. 
+        /// Distance is actualy the square of the distance to avoid the expensive sqrt operation. 
+        /// </summary>
+        /// <param name="sourceAsteroid"></param>
         private SortedDictionary<double, SortedList<double, Asteroid>> GetDictionaryOfNeighbours(Asteroid sourceAsteroid)
         {
             SortedDictionary<double, SortedList<double, Asteroid>> anglesWithNeighbors = new SortedDictionary<double, SortedList<double, Asteroid>>();
             foreach (Asteroid asteroid in this.Asteroids)
             {
                 if (asteroid == sourceAsteroid) { continue; }
-                double angle = sourceAsteroid.GetSlope(asteroid);
+                double angle = sourceAsteroid.GetAngle(asteroid);
 
                 if (anglesWithNeighbors.ContainsKey(angle) == false)
                 {
@@ -80,6 +101,12 @@ namespace Advent_of_Code.Day_10
             return anglesWithNeighbors;
         }
 
+
+        /// <summary>
+        /// When given an asteroid, calculate the number of asteroids visible (ie not occluded) from this object. 
+        /// Then update the "AsteroidsVisible" property on the input object with that count. 
+        /// </summary>
+        /// <param name="targetAsteroid"></param>
         internal void UpdateAsteroidsInFOV(Asteroid targetAsteroid)
         {
             HashSet<double> angles = new HashSet<double>();
@@ -87,13 +114,16 @@ namespace Advent_of_Code.Day_10
             {
                 if (asteroid == targetAsteroid) { continue; }
 
-                double slope = targetAsteroid.GetSlope(asteroid);
+                double slope = targetAsteroid.GetAngle(asteroid);
                 angles.Add(slope);
             }
 
             targetAsteroid.AsteroidsVisible = angles.Count;
         }
 
+        /// <summary>
+        /// Iterate accross all asteroids, and evaluate which has the greatest number of asteroids visible. 
+        /// </summary>
         internal Asteroid GetBestAsteroid()
         {
             Asteroid bestAsteroid = Asteroids[0];
