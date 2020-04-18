2f7953a193f5c6c7596110cc08550dd3a7f44290
Steve <sfry89@gmail.com>
2020-04-19 00:00:18 +0100

    fix: Broken LCM calculation
    LCM was broken from previous incomplete commit. Now fixed.

--- Advent of Code/SharedLibrary/MathLib.cs
+++ Advent of Code/SharedLibrary/MathLib.cs
@@ -5,18 +5,54 @@ using System.Linq;
 
 namespace Advent_of_Code.SharedLibrary
 {
-    public class MathLib
+    public static class MathLib
     {
-        public static double GetLCM(List<double> input) 
+        public static double GetGCD(double a, double b)
         {
-            int i = 1;
-            double maxVal = input.Max();
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            // Pull out remainders.
             while (true)
             {
-                double multiple = maxVal * i;
-                if (input.All(x => multiple % x == 0)) { return multiple; }
-                i++;
+                double remainder = a % b;
+                if (remainder == 0)
+                {
+                    return b;
+                }
+
+                a = b;
+                b = remainder;
+            };
+        }
+
+        public static double GetLCM(double a, double b)
+        {
+            return a / GetGCD(a, b) * b;
+        }
+
+
+        public static double GetLCM(List<double> input)
+        {
+            if (input.Count < 2)
+            {
+                throw new ArgumentException("Input must be a collection of 2 or more objects");
             }
+
+            double a = input[0];
+            double b = input[1];
+            List<double> remainingElements = input.Skip(2).ToList();
+            double result = GetLCM(a, b);
+
+            while (remainingElements.Count > 0)
+            {
+                a = result;
+                b = remainingElements[0];
+                remainingElements.RemoveAt(0);
+                result = GetLCM(a, b);
+            }
+
+            return result;
         }
     }
 }
--- Advent of Code/SharedLibrary/Tests/Test_MathLib.cs
+++ Advent of Code/SharedLibrary/Tests/Test_MathLib.cs
@@ -8,21 +8,39 @@ namespace Advent_of_Code.SharedLibrary.Tests
     public class Test_MathLib
     {
         [Theory]
-        [MemberData(nameof(ShouldCorrectlyCalculateLCM_Data))]
-        public void ShouldCorrectlyCalculateLCM(List<double> input, double expected)
+        [MemberData(nameof(ShouldCorrectlyCalculateLCM_TwoValues_Data))]
+        public void ShouldCorrectlyCalculateLCM_TwoValues(List<double> input, double expected)
         {
             double actual = SharedLibrary.MathLib.GetLCM(input);
-
             Assert.Equal(expected, actual);
         }
 
-        public static IEnumerable<object[]> ShouldCorrectlyCalculateLCM_Data() =>
+
+        [Theory]
+        [MemberData(nameof(ShouldCorrectlyCalculateLCM_MultipleValues_Data))]
+        public void ShouldCorrectlyCalculateLCM_MultipleValues(List<double> input, double expected)
+        {
+            double actual = SharedLibrary.MathLib.GetLCM(input);
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> ShouldCorrectlyCalculateLCM_MultipleValues_Data() =>
             new List<object[]>
             {
-                new object[] {new List<double> {14,22 }, 154 },
                 new object[] {new List<double> {12,15,10,75 }, 300},
-                new object[] {new List<double> {6,7,21 }, 42}
+                new object[] {new List<double> { 12, 18, 30 }, 180},
+                new object[] {new List<double> { 8, 18, 24 }, 72},
+                new object[] {new List<double> { 15, 20, 30, 35 }, 420},
+                new object[] {new List<double> { 16, 24, 40, 56 }, 1680},
+                new object[] {new List<double> { 16, 48, 64, 80, 112 }, 6720},
             };
 
+        public static IEnumerable<object[]> ShouldCorrectlyCalculateLCM_TwoValues_Data() =>
+            new List<object[]>
+            {
+                new object[] {new List<double> {8,12}, 24 },
+                new object[] {new List<double> {40,56}, 280 },
+                new object[] {new List<double> {14,22 }, 154 }
+            };
     }
 }
